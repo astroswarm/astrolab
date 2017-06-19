e888f529cf56e9a9d11e893867da7d81ecaa92a6
Robby Grossman <robby@freerobby.com>
2017-06-19 01:23:48 -0400

    Standardize Dockerfile using variable (x86_64 vs arm) base image.

---
+++ import_base_image.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+if [ $(uname -m) == "x86_64" ]; then
+  docker pull debian:latest
+  docker tag debian:latest base:latest
+elif uname -m | grep "arm" > /dev/null; then
+  docker pull resin/rpi-raspbian:jessie-20170614
+  docker tag resin/rpi-raspbian:jessie-20170614 base:latest
+else
+  echo "Invalid architecture"
+fi

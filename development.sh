537ca6e6e100fa61b55b53a64ef5de08accc4efd
Robby Grossman <robby@freerobby.com>
2018-05-20 22:35:05 -0400

    Use prebuilt images for each container.

--- development.sh
+++ development.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
 export ASTROSWARM_API_HOST=172.19.0.1:3001
-export BRAIN_CONTEXT=../brain
-export FRONTEND_CONTEXT=../frontend
-export FRONTEND_DOCKERFILE=Dockerfile.development
-export HEARTBEAT_CONTEXT=../heartbeat
 export HOST_DATA_DIR=/tmp
 export PORTAINER_DATA_DIR=/tmp
 export RACK_ENV=development
@@ -14,12 +10,6 @@ export WPA_SUPPLICANT_PATH=/tmp/wpa_supplicant.conf
 export PASTEBINIT_URI=http://$(ipconfig getifaddr en0):3002/
 export PASTEBINIT_USERNAME=astro
 export PASTEBINIT_PASS=swarm
-export BRAIN_GO_VERSION=1.10
-if [ "$(uname -m)" == "x86_64" ] ; then
-  export BRAIN_GO_ARCH=amd64
-elif [ "$(uname -m)" == "armv7l" ] ; then
-  export BRAIN_GO_ARCH=armv6l
-fi
 
 touch $WPA_SUPPLICANT_PATH
 echo -n $(ipconfig getifaddr en0) > $HOST_DATA_DIR/lan_ip_address
--- production.sh
+++ production.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
 export ASTROSWARM_API_HOST=api.astroswarm.com
-export BRAIN_CONTEXT=https://github.com/astroswarm/brain.git
-export FRONTEND_CONTEXT=https://github.com/astroswarm/frontend.git
-export FRONTEND_DOCKERFILE=Dockerfile.production
-export HEARTBEAT_CONTEXT=https://github.com/astroswarm/heartbeat.git
 export HOST_DATA_DIR=/host-data
 export PORTAINER_DATA_DIR=/mnt/shared/portainer/data
 export RACK_ENV=production
@@ -14,9 +10,4 @@ export WPA_SUPPLICANT_PATH=/etc/wpa_supplicant/wpa_supplicant.conf
 export PASTEBINIT_URI=http://pastebin.astroswarm.com/
 export PASTEBINIT_USERNAME=astro
 export PASTEBINIT_PASS=swarm
-export BRAIN_GO_VERSION=1.10
-if [ "$(uname -m)" == "x86_64" ] ; then
-  export BRAIN_GO_ARCH=amd64
-elif [ "$(uname -m)" == "armv7l" ] ; then
-  export BRAIN_GO_ARCH=armv6l
-fi
+
